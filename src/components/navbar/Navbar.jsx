e73f82fbe2654ed0012ce1e21ac07555ef45aab7
Maja Kadic <mkadic2@student.gdu.edu>
2024-04-11 02:39:48 -0400

    Front Page Updates 4/11/2024

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,7 +8,7 @@ import { signOut, onAuthStateChanged } from "firebase/auth";
 import { doc, getDoc } from "firebase/firestore"; // Import getDoc
 import { auth, db } from "../../config/firebaseConfig";
 
-const Navbar = () => {
+const Navbar = ({ isScrolled, howItWorksRef, aboutUsRef }) => {
     const [navbarScrolled, setNavbarScrolled] = useState(false);
     const [showLogin, setShowLogin] = useState(false);
     const [showSignUpOptions, setShowSignUpOptions] = useState(false);
@@ -19,6 +19,7 @@ const Navbar = () => {
     const adminPaths = ['/adminportal', '/adminportal/appointments', '/availability-manager'];
     const navigate = useNavigate();
     const location = useLocation();
+    const scrollToRef = (ref) => ref.current.scrollIntoView({ behavior: 'smooth', block: 'start' });
  
 
     useEffect(() => {
@@ -42,7 +43,7 @@ const Navbar = () => {
         });
 
         const handleScroll = () => {
-            const isScrolled = window.scrollY > 0; // Or whatever number you choose
+            const isScrolled = window.scrollY > 0;
             setNavbarScrolled(isScrolled);
         };
 
@@ -63,82 +64,100 @@ const Navbar = () => {
         navigate('/');
     };
 
-    return (
-        <div>
-            <nav className={`navbar ${navbarScrolled ? 'scrolled' : ''}`}>
-                <div className="navbar-logo">
-                    <img src="\assets\Viralidate Logo.png" alt="Logo" />
-                </div>
-                <ul className="navbar-menu">
-                    {patientPaths.includes(location.pathname) ? (
-                        <>
-                            <li><NavLink to="/">Home</NavLink></li>
-                            <li><NavLink to="/questionnaire">Symptom Checker</NavLink></li>
-                            <li><NavLink to="/">Appointments</NavLink></li>
-                            <li><a href="/">Records</a></li>
-                        </>
-                    ) : adminPaths.includes(location.pathname) ? (
-                        <>
-                            <li><NavLink to="/">Home</NavLink></li>
-                            <li><NavLink to="/availability-manager">Appointments</NavLink></li>
-                            <li><a href="/">Records</a></li>
-                        </>
-                    ) : (
-                        <>
-                            <li><NavLink to="/">Home</NavLink></li>
-                            <li><a href="/">Explore</a></li>
-                            <li><a href="/">About</a></li>
-                        </>
-                    )}
-                </ul>
-                <div className="navbar-login">
-                    {!isLoggedIn ? (
-                        <>
-                            <button onClick={() => setShowLogin(true)}>Login</button>
-                            <button onClick={() => setShowSignUpOptions(true)}>Sign Up</button>
-                        </>
-                    ) : (
-                        <>
-                            {/* Conditionally render portal link based on user profile */}
-                            {userProfileType === 'patient' ? (
-                                <Link to="/patientportal"><button>Portal</button></Link>
-                            ) : userProfileType === 'admin' ? (
-                                <Link to="/adminportal"><button>Portal</button></Link>
-                            ) : null}
-                            <button onClick={handleLogOut}>Log Out</button>
-                        </>
-                    )}
-                </div>
-            </nav>
-            {showLogin && (
-                <Login trigger={showLogin} setTrigger={setShowLogin} onLoginSuccess={() => setIsLoggedIn(true)} />
-            )}
+        // Function to scroll to the top of the page
+        const scrollToTop = () => {
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth',
+            });
+        };
 
-            {showSignUpOptions && (
-                <div className="popup">
-                    <div className="popup-inner">
-                        <button className="close-btn" onClick={() => setShowSignUpOptions(false)}>X</button>
-                        <div className="signup-options">
-                            <button className="signup-btn" onClick={() => handleSignUpOptionClick('patient')}>
-                                Patient Sign Up
-                            </button>
-                            <button className="signup-btn" onClick={() => handleSignUpOptionClick('admin')}>
-                                Admin Sign Up
-                            </button>
+            // Handling click for the Home link
+            const handleHomeClick = (e) => {
+                e.preventDefault(); 
+                if (location.pathname !== '/') {
+                    navigate('/'); // Navigate to homepage if not already there
+                }
+                scrollToTop(); 
+            };
+
+            return (
+                <div>
+                    <nav className={`navbar ${navbarScrolled ? 'scrolled' : ''}`}>
+                        <div className="navbar-logo">
+                            <Link to="/" onClick={scrollToTop}>
+                                <img src="\assets\Viralidate Logo.png" alt="Logo" />
+                            </Link>
                         </div>
-                    </div>
+                        <ul className="navbar-menu">
+                            {/* Only render the Home link if the user is not on the homepage */}
+                            {location.pathname !== '/' && (
+                                <li>
+                                    <NavLink to="/" onClick={scrollToTop}>Home</NavLink>
+                                </li>
+                            )}
+                            <li><a href="#how-it-works" onClick={(e) => {e.preventDefault(); howItWorksRef.current?.scrollIntoView({ behavior: 'smooth' })}}>Explore</a></li>
+                            <li><a href="#about-us" onClick={(e) => {e.preventDefault(); aboutUsRef.current?.scrollIntoView({ behavior: 'smooth' })}}>About</a></li>
+                            {patientPaths.includes(location.pathname) && (
+                                <>
+                                    <li><NavLink to="/questionnaire">Symptom Checker</NavLink></li>
+                                    <li><NavLink to="/">Appointments</NavLink></li>
+                                    <li><a href="/">Records</a></li>
+                                </>
+                            )}
+                            {adminPaths.includes(location.pathname) && (
+                                <>
+                                    <li><NavLink to="/availability-manager">Appointments</NavLink></li>
+                                    <li><a href="/">Records</a></li>
+                                </>
+                            )}
+                        </ul>
+                        <div className="navbar-login">
+                            {!isLoggedIn ? (
+                                <>
+                                    <button onClick={() => setShowLogin(true)}>Login</button>
+                                    <button onClick={() => setShowSignUpOptions(true)}>Sign Up</button>
+                                </>
+                            ) : (
+                                <>
+                                    {userProfileType === 'patient' ? (
+                                        <Link to="/patientportal"><button>Portal</button></Link>
+                                    ) : userProfileType === 'admin' ? (
+                                        <Link to="/adminportal"><button>Portal</button></Link>
+                                    ) : null}
+                                    <button onClick={handleLogOut}>Log Out</button>
+                                </>
+                            )}
+                        </div>
+                    </nav>
+                    {showLogin && (
+                        <Login trigger={showLogin} setTrigger={setShowLogin} onLoginSuccess={() => setIsLoggedIn(true)} />
+                    )}
+                    {showSignUpOptions && (
+                        <div className="popup">
+                            <div className="popup-inner">
+                                <button className="close-btn" onClick={() => setShowSignUpOptions(false)}>X</button>
+                                <div className="signup-options">
+                                    <button className="signup-btn" onClick={() => handleSignUpOptionClick('patient')}>
+                                        Patient Sign Up
+                                    </button>
+                                    <button className="signup-btn" onClick={() => handleSignUpOptionClick('admin')}>
+                                        Admin Sign Up
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    )}
+                    {signUpType === 'patient' && (
+                        <SignUp trigger={true} setTrigger={() => setSignUpType("")} type="patient" />
+                    )}
+                    {signUpType === 'admin' && (
+                        <SignUp trigger={true} setTrigger={() => setSignUpType("")} type="admin" />
+                    )}
                 </div>
-            )}
-
-            {signUpType === 'patient' && (
-                <SignUp trigger={true} setTrigger={() => setSignUpType("")} type="patient" />
-            )}
-
-            {signUpType === 'admin' && (
-                <SignUp trigger={true} setTrigger={() => setSignUpType("")} type="admin" />
-            )}
-        </div>
-    )
+            );
+            
+    
 }
 
 export default Navbar;
--- src/pages/frontpage/Frontpage.jsx
+++ src/pages/frontpage/Frontpage.jsx
@@ -9,6 +9,7 @@ const Frontpage = () => {
     const [isScrolled, setIsScrolled] = useState(false);
     const howItWorksRef = useRef(null); 
     const [showArrow, setShowArrow] = useState(true);
+    const aboutUsRef = useRef(null); 
 
 
     useEffect(() => {
@@ -39,7 +40,7 @@ const Frontpage = () => {
 
     return (
         <div>
-            <Navbar isScrolled={isScrolled} />
+            <Navbar isScrolled={isScrolled} howItWorksRef={howItWorksRef} aboutUsRef={aboutUsRef} />
             <div className="hero">
                 <div className="video-container">
                     <iframe 
@@ -50,13 +51,17 @@ const Frontpage = () => {
                             left: '0',
                             width: '100%',
                             height: '100%',
-                            frameborder: '0',
+                            frameBorder: '0',
                             allow: 'autoplay; fullscreen; picture-in-picture; clipboard-write',
-                            allowfullscreen: true,
+                            allowFullScreen: true,
                             title: 'New Video'
                         }}
-                    ></iframe>
-                </div>
+                        ></iframe>
+                        <div className="text-block">
+                        <p className="top-text">Revolutionizing Healthcare</p>
+                        <p className="bottom-text">Smart Symptom Checking and Appointment Scheduling at Your Fingertips</p>
+                        </div>
+                    </div>
                 <div className="footer-navbar">
                 <img
                     src="/assets/arrow.png"
@@ -71,42 +76,43 @@ const Frontpage = () => {
             <h2 className="how-it-works-heading">How It Works</h2>
                 <HowItWorks />
             </div>
-<h2 className="about-us-heading">About Us</h2>
-    <div className="about-us-container">
-      <div className="about-us-content">
-        <div className="about-us-text">
-          <p>
-Viralidate is revolutionizing healthcare with our innovative platform that automates symptom triage and appointment scheduling for healthcare institutions. Our solution supports the core of the healthcare system, easing the workload of professionals and staff by optimizing patient care and resource allocation. Our advanced algorithm precisely assesses symptoms, manages healthcare providers' capacities, and prioritizes appointments, enhancing efficiency and patient outcomes.
-          </p>
-          <p>We are dedicated to innovation and continuously adapting to meet the future demands of healthcare delivery. Viralidate is more than a technological tool; it's a pledge to improve healthcare experiences, ensuring operational excellence and fostering better outcomes for both patients and providers.
-          </p>
-        </div>
-        <div className="about-us-image">
-          <img src="\assets\team.jpg" alt="About Us" />
-        </div>
-      </div>
-    </div>
-
-  <footer className="footer-section">
-                <div className="footer-content">
-                    <div className="footer-left">
-                    <img src="\assets\Viralidate Logo.png" alt="Company Logo" />
-                        <p>Georgia State University, 33 Gilmer Street SE, Atlanta, GA 30303</p>
-                    </div>
-                    <div className="footer-right">
-                        <h3>Newsletter</h3>
-                        <p>Sign up for our newsletter for essential tips and news.</p>
-                        <input type="email" placeholder="Your email" />
-                        <button>Send</button>
-                    </div>
+            <h2 className="about-us-heading" ref={aboutUsRef}>About Us</h2>
+            <div className="about-us-container">
+              <div className="about-us-content">
+                <div className="about-us-text">
+                  <p>
+        Viralidate is revolutionizing healthcare with our innovative platform that automates symptom triage and appointment scheduling for healthcare institutions. Our solution supports the core of the healthcare system, easing the workload of professionals and staff by optimizing patient care and resource allocation. Our advanced algorithm precisely assesses symptoms, manages healthcare providers' capacities, and prioritizes appointments, enhancing efficiency and patient outcomes.
+                  </p>
+                  <p>We are dedicated to innovation and continuously adapting to meet the future demands of healthcare delivery. Viralidate is more than a technological tool; it's a pledge to improve healthcare experiences, ensuring operational excellence and fostering better outcomes for both patients and providers.
+                  </p>
                 </div>
-                <div className="footer-bottom">
-                    <p>Made in Atlanta</p>
-                    <p>© 2024 Viralidate. All rights reserved.</p>
+                <div className="about-us-image">
+                  <img src="\assets\team.jpg" alt="About Us" />
                 </div>
-            </footer>
-        </div>
+              </div>
+            </div>
+    
+          <footer className="footer-section">
+                        <div className="footer-content">
+                            <div className="footer-left">
+                            <img src="\assets\Viralidate Logo.png" alt="Company Logo" />
+                                <p>Georgia State University, 33 Gilmer Street SE, Atlanta, GA 30303</p>
+                            </div>
+                            <div className="footer-right">
+                                <h3>Newsletter</h3>
+                                <p>Sign up to our newsletter for essential tips and news.</p>
+                                <input type="email" placeholder="Your email" />
+                                <button>Send</button>
+                            </div>
+                        </div>
+                        <div className="footer-bottom">
+                            <p>Made in Atlanta</p>
+                            <p>© 2024 Viralidate. All rights reserved.</p>
+                        </div>
+                    </footer>
+                </div>
     );
+    
 };
 
 export default Frontpage;
