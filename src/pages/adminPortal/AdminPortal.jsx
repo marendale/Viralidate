34a2616f94e89dd6b657b689af90f5a41d0ae7de
Maja Kadic <mkadic2@student.gdu.edu>
2024-04-11 05:26:28 -0400

    Patient & Admin Portals

--- src/pages/adminPortal/AdminPortal.jsx
+++ src/pages/adminPortal/AdminPortal.jsx
@@ -1,16 +1,34 @@
-/* eslint-disable no-unused-vars */
-import React from 'react'
-import './AdminPortal.css'
+import React from 'react';
+import './AdminPortal.css';
 import Navbar from '../../components/navbar/Navbar';
+import { FaUser, FaFileMedical, FaCalendarCheck, FaEnvelope, FaStethoscope } from 'react-icons/fa';
 
 
 const AdminPortal = () => {
     return (
-        <div>
+        <div className="admin-portal-container">
             <Navbar />
-            <div className="adminportal"></div>
+            <div className="portal-content">
+                <aside className="sidebar">
+                <ul className="menu-list">
+                        <li><FaUser className="icon" /> Admin Profile</li>
+                        <li><FaFileMedical className="icon" /> Admin Records</li>
+                        <li><FaCalendarCheck className="icon" /> Appointment Manager</li>
+                        <li><FaEnvelope className="icon" /> Messages</li>
+                        <li><FaStethoscope className="icon" /> Symptom Checker</li>
+                    </ul>
+                </aside>
+                <main className="main-content">
+                    <section className="upcoming-visits">
+                        <h2>Upcoming Visits</h2>
+                        {/* Content for upcoming visits */}
+                    </section>
+                    {/* main content specific to the selected menu option */}
+                </main>
+            </div>
         </div>
-    )
+    );
 }
 
 export default AdminPortal;
+
--- src/pages/patientPortal/PatientPortal.jsx
+++ src/pages/patientPortal/PatientPortal.jsx
@@ -1,14 +1,34 @@
-import React from 'react'
-import './PatientPortal.css'
+import React from 'react';
+import './PatientPortal.css';
 import Navbar from '../../components/navbar/Navbar';
+import { FaUser, FaFileMedical, FaCalendarCheck, FaEnvelope, FaStethoscope } from 'react-icons/fa';
+
 
 const PatientPortal = () => {
     return (
-        <div>
+        <div className="patient-portal-container">
             <Navbar />
-            <div className="patientportal"></div>
+            <div className="portal-content">
+                <aside className="sidebar">
+                <ul className="menu-list">
+                        <li><FaUser className="icon" /> Patient Profile</li>
+                        <li><FaFileMedical className="icon" /> Patient Records</li>
+                        <li><FaCalendarCheck className="icon" /> Appointment Manager</li>
+                        <li><FaEnvelope className="icon" /> Messages</li>
+                        <li><FaStethoscope className="icon" /> Symptom Checker</li>
+                    </ul>
+                </aside>
+                <main className="main-content">
+                    <section className="upcoming-visits">
+                        <h2>Upcoming Visits</h2>
+                          {/* Content for upcoming visits */}
+                    </section>
+                    {/* main content specific to the selected menu option */}
+                </main>
+            </div>
         </div>
-    )
+    );
 }
 
 export default PatientPortal;
+
